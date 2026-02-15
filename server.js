7bea25b9a6864fe4e2876368e3ffdd090b327d67
2009ejsvk <2009ejsvk@gmail.com>
2026-02-15 22:25:32 +0900

    index.hmtl

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const app = express();
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/api/health", (_req, res) => {
   res.json({
@@ -177,7 +178,7 @@ app.use((req, res, next) => {
 });
 
 app.get("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.use((error, _req, res, _next) => {
